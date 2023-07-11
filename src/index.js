17ed84b041b8413c2c0ce63283493e320c383fb7
djns <130430285+DDJCS50@users.noreply.github.com>
2023-07-11 12:19:59 -0700

    Add sidebar rendering funcs and styling
    Added funcs to the page.js file for rendering the different tabs for
    project sections. Added styling to make it look better.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import { initialRender } from './page';
 
 initialRender.renderLogo();
 initialRender.renderPlus();
+initialRender.renderDueDates('Today');
+initialRender.renderDueDates('Upcoming');
+initialRender.renderDueDates('Someday');
+initialRender.renderProjectSections('Family');
+initialRender.renderProject('Vacation in Rome');
+initialRender.renderProjectSections('Work');
+initialRender.renderProject('Prepare Presentation');
+initialRender.renderProjectSections('Hobbies');
+initialRender.renderProject('Learn Basic Italian');
+initialRender.renderAddList();
--- src/page.js
+++ src/page.js
@@ -3,6 +3,7 @@ import plus from './plus.svg';
 
 const homePage = () => {
   const header = document.querySelector('header');
+  const sidebar = document.querySelector('#sidebar');
 
   function renderLogo() {
     const logo = document.createElement('img');
@@ -13,15 +14,83 @@ const homePage = () => {
 
   function renderPlus() {
     const plus = document.createElement('img');
+    const plusText = document.createElement('p');
+    plusText.innerText = 'New Task';
     plus.src = './images/plus.svg';
     plus.alt = 'Plus sign';
-    plus.id = 'plus';
+    plus.id = 'plusTask';
     header.appendChild(plus);
+    header.appendChild(plusText);
+  }
+
+  function renderDueDates(section) {
+    const dueDateDiv = document.createElement('div');
+    dueDateDiv.id = section.toLowerCase();
+
+    const symbol = document.createElement('img');
+    symbol.alt = '#';
+
+    const title = document.createElement('p');
+    title.innerText = section;
+
+    dueDateDiv.appendChild(symbol);
+    dueDateDiv.appendChild(title);
+    sidebar.appendChild(dueDateDiv);
+  }
+
+  function renderProjectSections(section) {
+    const projectSection = document.createElement('div');
+    projectSection.id = section;
+    projectSection.classList = 'projectSection';
+
+    const symbol = document.createElement('img');
+    symbol.alt = '#';
+
+    const title = document.createElement('h2');
+    title.innerText = section;
+
+    projectSection.appendChild(symbol);
+    projectSection.appendChild(title);
+    sidebar.appendChild(projectSection);
+  }
+
+  function renderProject(section) {
+    const project = document.createElement('div');
+    project.id = section;
+    project.classList = 'project';
+
+    const symbol = document.createElement('img');
+    symbol.alt = '#';
+
+    const title = document.createElement('p');
+    title.innerText = section;
+
+    project.appendChild(symbol);
+    project.appendChild(title);
+    sidebar.appendChild(project);
+  }
+
+  function renderAddList() {
+    const newList = document.createElement('div');
+    newList.id = 'newList';
+    const plus = document.createElement('img');
+    const plusText = document.createElement('p');
+    plusText.innerText = 'New List';
+    plus.src = './images/plus.svg';
+    plus.alt = 'Plus sign';
+    plus.id = 'plusList';
+    newList.appendChild(plus);
+    newList.appendChild(plusText);
+    sidebar.appendChild(newList);
   }
 
   return {
     renderLogo,
-    renderPlus
+    renderPlus,
+    renderDueDates,
+    renderProjectSections,
+    renderProject,
+    renderAddList
   }
 }
 export const initialRender = homePage();
